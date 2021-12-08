ccd9cb00f28a0b2ab02d8c1974b2f350953cea07
shyam2520 <shyam2562000@gmail.com>
2021-12-08 21:46:53 +0530

    CORS

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ collection=db['Show_Name_Details']
 
 app =FastAPI()
 
-origins=['http://localhost:3000/']
+# origins=['http://localhost:3000/']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
